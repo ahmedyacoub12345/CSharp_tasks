0a23d436f47f68c46c9934d54042ccd91c18be9c
ahmedyacoub12345 <ahmedyacoub718@gmail.com>
2024-06-26 20:52:22 +0300

    Task2

---
+++ Cohort4-Task2/Cohort4-Task2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cohort4_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 =int.Parse( Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int smallerNumber = Math.Min(num1, num2);
+            //Console.WriteLine("The smaller number is: " + smallerNumber);
+            //Console.WriteLine("*************************************");
+
+            ///////////////////////////////////////////////////////////////////
+
+            //int number = int.Parse(Console.ReadLine());
+            //string sign = (number > 0) ? "+" : (number < 0) ? "-" : "0";
+            //Console.WriteLine("The sign is " + sign);
+            //Console.WriteLine("*************************************");
+
+            //////////////////////////////////////////////////////////////////
+
+            //int[] numbers = { 0, -1, 4 };
+            //Array.Sort(numbers);
+            //Array.Reverse(numbers);
+            //Console.WriteLine("Sorted numbers: " + string.Join(", ", numbers));
+            //Console.WriteLine("*************************************");
+
+            ////////////////////////////////////////////////////////////////
+
+            //int[] numbers = { -5, -2, -6, 0, -1 };
+            //int maxNumber = Math.Max(numbers[0], Math.Max(numbers[1], Math.Max(numbers[2], Math.Max(numbers[3], numbers[4]))));
+            //Console.WriteLine("maxNumber is: " + maxNumber);
+            //Console.WriteLine("*************************************");
+
+            //////////////////////////////////////////////////////////////////
+
+            //double kilometersPerHour = double.Parse(Console.ReadLine());
+            //double milesPerHour = kilometersPerHour * 0.621371;
+            //Console.WriteLine($"{kilometersPerHour} kilometers per hour is {milesPerHour} miles per hour");
+            //Console.WriteLine("*************************************");
+
+            //////////////////////////////////////////////////////////////
+
+            //int hours = int.Parse(Console.ReadLine());
+            //int minutes = int.Parse(Console.ReadLine());
+            //int totalMinutes = (hours * 60) + minutes;
+            //Console.WriteLine($"Total: {totalMinutes} minutes.");
+            //Console.WriteLine("*************************************");
+
+            ////////////////////////////////////////////////////////////
+
+            //int totalMinutes = int.Parse(Console.ReadLine());
+            //int hours = totalMinutes / 60;
+            //int minutes = totalMinutes % 60;
+            //Console.WriteLine($"{hours} Hours, {minutes} Minutes");
+            //Console.WriteLine("*************************************");
+
+            ////////////////////////////////////////////////////////
+
+            //string[] sentences = {
+            //"This is a short sentence.",
+            //"This is a bit longer sentence that we have here.",
+            //"Short one.",
+            //"An even longer sentence that certainly exceeds the usual length for a sentence in this example.",
+            //"Tiny."
+            //};
+            //int fixedLength = 10;
+            //Console.WriteLine(sentences[0].PadRight(fixedLength).Substring(0, fixedLength));
+            //Console.WriteLine(sentences[1].PadRight(fixedLength).Substring(0, fixedLength));
+            //Console.WriteLine(sentences[2].PadRight(fixedLength).Substring(0, fixedLength));
+            //Console.WriteLine(sentences[3].PadRight(fixedLength).Substring(0, fixedLength));
+            //Console.WriteLine(sentences[4].PadRight(fixedLength).Substring(0, fixedLength));
+            //Console.WriteLine("*************************************");
+
+
+
+
+        }
+    }
+}
